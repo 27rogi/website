6310067e78ed5c339f038739b22ce173d1702b41
rogi27 <27rogi@pm.me>
2023-04-03 00:03:11 +0300

    feat: use Nitro compression

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,12 +11,18 @@ export default defineNuxtConfig({
       branch: 'master',
     },
   },
+  nitro: {
+    compressPublicAssets: true,
+  },
   app: {
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'canonical', href: 'https://ryzhenkov.space' },
+      ],
     },
   },
   vite: {
