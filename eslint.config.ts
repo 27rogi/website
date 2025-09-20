c03f7194a53cf5e480f2bacf678e9d7012e74f69
27rogi <27rogi@pm.me>
2025-09-20 16:58:17 +0300

    chore(eslint): tweak eslint settings

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,23 +1,30 @@
-import antfu from '@antfu/eslint-config'
-import sort from 'eslint-plugin-sort'
-import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from "@antfu/eslint-config"
+import sort from "eslint-plugin-sort"
+import withNuxt from "./.nuxt/eslint.config.mjs"
 
 export default withNuxt(antfu({
   formatters: true,
-  stylistic: true,
+  stylistic: {
+    indent: 2,
+    quotes: "double",
+  },
   unocss: true,
   vue: true,
 }).append()).append(
-  sort.configs['flat/recommended'],
+  sort.configs["flat/recommended"],
 ).overrideRules({
-  '@stylistic/indent': ['error', 2],
-  '@stylistic/quotes': ['error', 'double'],
-  'sort/imports': 'off',
-  'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-  'vue/singleline-html-element-content-newline': [
-    'warn',
+  "@stylistic/indent": ["error", 2],
+  "@stylistic/quotes": ["error", "double"],
+  "sort/imports": "off",
+  "vue/component-name-in-template-casing": ["error", "PascalCase"],
+  "vue/first-attribute-linebreak": ["warn", {
+    multiline: "ignore",
+    singleline: "ignore",
+  }],
+  "vue/singleline-html-element-content-newline": [
+    "warn",
     {
-      ignores: ['textarea', 'pre', 'p', 'span'],
+      ignores: ["textarea", "pre", "p", "span"],
     },
   ],
 })
