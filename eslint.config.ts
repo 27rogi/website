dfe220749e9c114cab7dd2bdc7beab10f6ea7161
27rogi <27rogi@pm.me>
2025-09-26 21:25:22 +0300

    chore(eslint): append nuxt instead of wrap

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,8 @@
 import antfu from "@antfu/eslint-config"
 import sort from "eslint-plugin-sort"
-import withNuxt from "./.nuxt/eslint.config.mjs"
+import nuxt from "./.nuxt/eslint.config.mjs"
 
-export default withNuxt(antfu({
+export default antfu({
   formatters: true,
   stylistic: {
     indent: 2,
@@ -10,21 +10,22 @@ export default withNuxt(antfu({
   },
   unocss: true,
   vue: true,
-}).append()).append(
-  sort.configs["flat/recommended"],
-).overrideRules({
-  "@stylistic/indent": ["error", 2],
-  "@stylistic/quotes": ["error", "double"],
-  "sort/imports": "off",
-  "vue/component-name-in-template-casing": ["error", "PascalCase"],
-  "vue/first-attribute-linebreak": ["warn", {
-    multiline: "ignore",
-    singleline: "ignore",
-  }],
-  "vue/singleline-html-element-content-newline": [
-    "warn",
-    {
-      ignores: ["textarea", "pre", "p", "span"],
-    },
-  ],
 })
+  .append(nuxt())
+  .append(sort.configs["flat/recommended"])
+  .overrideRules({
+    "@stylistic/indent": ["error", 2],
+    "@stylistic/quotes": ["error", "double"],
+    "sort/imports": "off",
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "vue/first-attribute-linebreak": ["warn", {
+      multiline: "ignore",
+      singleline: "ignore",
+    }],
+    "vue/singleline-html-element-content-newline": [
+      "warn",
+      {
+        ignores: ["textarea", "pre", "p", "span"],
+      },
+    ],
+  })
