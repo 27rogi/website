31377470a4af33a038977b01e1ddf2e72636daa1
27rogi <27rogi@pm.me>
2026-04-09 22:12:40 +0000

    fix: use defineSitemapSchema and fix zod for it to work

--- content.config.ts
+++ content.config.ts
@@ -1,37 +1,40 @@
 import { defineCollection, defineContentConfig } from "@nuxt/content"
-import { asSitemapCollection } from "@nuxtjs/sitemap/content"
+import { defineSitemapSchema } from "@nuxtjs/sitemap/content"
 import { z } from "zod/v4"
 
-const reviewSchema = z.object({
-  categories: z.array(z.string()),
-  createdAt: z.date(),
-  daysInUse: z.number().min(0),
-  description: z.string(),
-  icon: z.string(),
-  rating: z.number().min(1).max(5),
-  title: z.string(),
-  updatedAt: z.date(), 
-})
+// TODO: implement schema for reviews
+// const reviewSchema = z.object({
+//   categories: z.array(z.string()),
+//   createdAt: z.date(),
+//   daysInUse: z.number().min(0),
+//   description: z.string(),
+//   icon: z.string(),
+//   rating: z.number().min(1).max(5),
+//   title: z.string(),
+//   updatedAt: z.date(),
+// })
 
 export default defineContentConfig({
   collections: {
-    reviews_en: defineCollection(asSitemapCollection({
-      schema: reviewSchema,
+    reviews_en: defineCollection({
+      schema: z.object({
+        sitemap: defineSitemapSchema(),
+      }),
       source: {
         include: "reviews/en/**",
         prefix: "/reviews/",
       },
       type: "page",
     }),
-    ),
-    reviews_ru: defineCollection(asSitemapCollection({
-      schema: reviewSchema,
+    reviews_ru: defineCollection({
+      schema: z.object({
+        sitemap: defineSitemapSchema(),
+      }),
       source: {
         include: "reviews/ru/**",
         prefix: "/reviews/",
       },
       type: "page",
     }),
-    ),
   },
 })
